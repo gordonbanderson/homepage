e7ace3fdca5a802c183e4737a39a02b48237e008
Gordon Anderson <gordon.b.anderson@gmail.com>
2018-03-16 14:34:04 +0700

    Base pages with controllers

---
+++ src/Controller/HomePageController.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: gordon
+ * Date: 16/3/2561
+ * Time: 3:43 น.
+ */
+
+namespace Suilven\HomeLandingPage\Model;
+
+class HomePageController extends \PageController
+{
+
+}
---
+++ src/Model/HomePage.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: gordon
+ * Date: 16/3/2561
+ * Time: 3:43 น.
+ */
+
+namespace Suilven\HomeLandingPage\Model;
+
+
+class HomePage extends \Page
+{
+
+}
